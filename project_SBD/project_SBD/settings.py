a4922ca8618117b178267b585c13113271f9acdf
Phuong <np384874@gmail.com>
2026-05-10 10:42:12 +0700

    chore: Use cloud db instead sqlite3

--- project_SBD/project_SBD/settings.py
+++ project_SBD/project_SBD/settings.py
@@ -83,6 +83,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
